f6eefe9884a0c5a801c90a5f68286eb1e12b6669
Julekha5 <mulani.julekha93@gmail.com>
2021-12-09 13:52:06 +0530

    Added:More than 3 parameter

--- src/genericpack/Maximum.java
+++ src/genericpack/Maximum.java
@@ -1,47 +1,25 @@
 package genericpack;
+import java.util.Arrays;
+import java.util.List;
 
-import java.util.Scanner;
+public class Maximum<T extends Comparable<T>> {
 
-public class Maximum<T> {
+	public static <T extends Comparable<T>> T findMax(List<T> arr) {
 
-	T a;
-	T b;
-	T c;
-
-	/**
-	 * @param a
-	 * @param b
-	 * @param c
-	 */
-	public Maximum(T a, T b, T c) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	/*
-	 * Method for finding maximum number (int,float,string)
-	 */
-	public static <T extends Comparable<T>> T findMax(T a, T b, T c) {
-
-		T max = a;
-
-		if (b.compareTo(max) > 0) {
-			max = b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = c;
+		T max = arr.get(0);
+		for (T key : arr) {
+			if (key.compareTo(max) > 0)
+				max = key;
 		}
 		return max;
 	}
 
 	public static void main(String[] args) {
-		Maximum mx = new Maximum(args, args, args);
-		System.out.println("Welcome to Generic Practice Problem");
-		System.out.println("The Max Integer is:" + mx.findMax(7, 8, 9));
-		System.out.println("The Max Float is:" + mx.findMax(4.6, 5.3, 6.7));
-		System.out.println("The Max String is:" + mx.findMax("apple", "banana", "peach"));
+		Maximum obj = new Maximum();
+		System.out.println("The Maximum integer num is:" + obj.findMax(Arrays.asList(4, 7, 8, 9, 44, 55)));
+		System.out.println("The Maximum float num is:" + obj.findMax(Arrays.asList(1.9, 3.0, 6.7, 2.6, 4.5, 5.8)));
+		System.out.println(
+				"The Maximum String  is:" + obj.findMax(Arrays.asList("Apple", "banana", "peach", "mango", "kiwi")));
 
 	}
 }
